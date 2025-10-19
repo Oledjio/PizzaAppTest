fa3c18923f43326faf845a5533c9d32ccc88e8e6
Oleg Seliverstov <oleg.seliverstov94@gmail.com>
2025-10-19 20:38:53 +0200

    add validation

--- src/app/layouts/components/create-order/create-order.component.ts
+++ src/app/layouts/components/create-order/create-order.component.ts
@@ -1,15 +1,28 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-create-order',
     templateUrl: './create-order.component.html',
     styleUrl: './create-order.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [ReactiveFormsModule, FormsModule],
+    imports: [ReactiveFormsModule, FormsModule, NgTemplateOutlet],
 })
 export class CreateOrderComponent implements OnInit {
     public form: FormGroup | undefined;
+
+    get nameControl(): AbstractControl {
+        return this.form?.get('name') as AbstractControl;
+    }
+
+    get addressControl(): AbstractControl {
+        return this.form?.get('address') as AbstractControl;
+    }
+
+    get phoneControl(): AbstractControl {
+        return this.form?.get('phone') as AbstractControl;
+    }
 
     constructor(private readonly _formBuilder: FormBuilder) {}
 
@@ -18,6 +31,11 @@ export class CreateOrderComponent implements OnInit {
     }
 
     public submitForm(): void {
+        if (!this.form?.valid) {
+            this.form?.markAllAsTouched();
+            return;
+        }
+
         console.log(this.form?.value);
     }
 
